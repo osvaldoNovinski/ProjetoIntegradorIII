8361780da827e0dd55c4b6c841cd6f6ff2f8ca28
osvaldoNovinski <37417572+osvaldoNovinski@users.noreply.github.com>
2018-04-26 22:56:04 -0300

    Arquivos

---
+++ ProjetoIntegradorIII/src/br/edu/unifacear/dao/CategoriaDAO.java
@@ -0,0 +1,93 @@
+package br.edu.unifacear.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.unifacear.model.Categoria;
+
+public class CategoriaDAO extends DAO {
+	
+	private String SQL_INSERT = "INSERT INTO TB_CATEGORIA (id_cat, nome_cat, status_cat) values (?,?,?);";
+	private String SQL_UPDATE = "UPDATE TB_CATEGORIA SET nome_cat = ?, status_cat = ? WHERE id_cat=?;";
+	private String SQL_SELECT = "SELECT * FROM TB_CATEGORIA;";
+	private String SQL_DELET = "UPDATE TB_CATEGORIA SET status_cat = ? WHERE id = ?;";
+	
+	
+public void inserir (Categoria cat) {
+	try {
+		conectar();
+		
+		PreparedStatement ps = db.getConnection().prepareStatement(SQL_INSERT);
+		
+		ps.setInt(1, cat.getId());
+		ps.setString(2, cat.getNome());
+		ps.setBoolean(3, cat.isStatus());
+		
+		ps.executeUpdate();
+		
+		desconectar();
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+}
+
+public void alterar (Categoria cat) {
+	try {
+		conectar();
+		
+		PreparedStatement ps = db.getConnection().prepareStatement(SQL_UPDATE);
+		
+		ps.setString(1, cat.getNome());
+		ps.setBoolean(2, cat.isStatus());
+		ps.setInt(3, cat.getId());
+		
+		
+		ps.executeUpdate();
+		
+		desconectar();
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+}
+
+public List<Categoria> listarTodos(){
+	List<Categoria> lista = new ArrayList<Categoria>();
+	
+	try {
+		conectar();
+		
+		PreparedStatement ps = db.getConnection().prepareStatement(SQL_SELECT);
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			Categoria catego = new Categoria(rs.getInt("id_cat"),rs.getString("nome_cat"),rs.getBoolean("status_cat"));
+			
+			lista.add(catego);
+		}
+	}catch (Exception e) {
+		e.printStackTrace();
+	}
+	
+	return lista;
+}
+
+public void excluir (Categoria cat) {
+	try {
+		conectar();
+		
+		PreparedStatement ps = db.getConnection().prepareStatement(SQL_DELET);
+		
+		ps.setBoolean(1, false);
+		ps.setInt(2, cat.getId());
+		
+		ps.executeUpdate();
+		
+		desconectar();
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+}
+
+}
